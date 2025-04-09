3e8ef479e444d6c088e9960d44ee99c416889291
Nisse Bergman <nibe91@gmail.com>
2025-04-09 09:41:04 +0200

    test

--- test/squared.test.ts
+++ test/squared.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from 'vitest'
 import { squared } from '../src/squared.js'
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(squared(2)).toBe(4)
+  expect(squared(2)).toBe(5)
 })
